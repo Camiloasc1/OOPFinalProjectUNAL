82a62ec43c70e42cbb876103e15d65813dce43fb
Camiloasc1 <caasanchezcu@unal.edu.co>
2014-01-04 11:14:29 -0500

    Best rectangle draw

--- Stratego/src/gui/util/DrawUtil.java
+++ Stratego/src/gui/util/DrawUtil.java
@@ -33,14 +33,12 @@ public final class DrawUtil
 		}
 	}
 	
-	public static void drawFlashRectangle(Rectangle rtg)
+	public static void drawRectangle(Rectangle rtg, byte red, byte green, byte blue, byte alpha)
 	{
-		// flashAlpha();
-		
 		GL11.glPushMatrix();
 		GL11.glTranslatef(rtg.x, rtg.y, 0);
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
-		GL11.glColor4ub((byte) 255, (byte) 255, (byte) 255, (byte) flashAlpha);
+		GL11.glColor4ub(red, green, blue, alpha);
 		GL11.glBegin(GL11.GL_QUADS);
 		{
 			GL11.glTexCoord2f(0, 0);
@@ -54,6 +52,16 @@ public final class DrawUtil
 		}
 		GL11.glEnd();
 		GL11.glPopMatrix();
+	}
+	
+	public static void drawFlashRectangle(Rectangle rtg, byte red, byte green, byte blue)
+	{
+		drawRectangle(rtg, red, green, blue, (byte) flashAlpha);
+	}
+	
+	public static void drawFlashRectangle(Rectangle rtg)
+	{
+		drawFlashRectangle(rtg, (byte) 255, (byte) 255, (byte) 255);
 	}
 	
 	private DrawUtil()
