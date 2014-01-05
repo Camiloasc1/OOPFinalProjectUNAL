df80dc4f36209bea9704ae478f98715271a9e2ec
Camiloasc1 <caasanchezcu@unal.edu.co>
2014-01-05 12:16:53 -0500

    Added License

--- Stratego/src/gui/Sprite.java
+++ Stratego/src/gui/Sprite.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * File: Sprite.java
+ * Project: Stratego
+ * 
+ * Copyright (C) 2014 Camiloasc1.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 
 package gui;
 
@@ -59,13 +78,14 @@ public class Sprite implements Serializable
 	{
 		return texture.getImageHeight();
 	}
-
+	
 	/**
 	 * @return the sprite rectangle
 	 */
 	public Rectangle getRectangle(int x, int y)
 	{
-		return new Rectangle(x * texture.getImageWidth(), y * texture.getImageWidth(), texture.getImageWidth(), texture.getImageHeight());
+		return new Rectangle(x * texture.getImageWidth(), y * texture.getImageWidth(), texture.getImageWidth(),
+				texture.getImageHeight());
 	}
 	
 	/**
